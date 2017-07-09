0b3f8508b576ecf28da64347c3a7cfbe73285564
emperorjm <emperor_jm@yahoo.com>
2017-07-09 10:24:00 -0500

    Update 2014_10_12_200000_create_subscriptions_table.php
    Because Laravel Spark already created a subscriptions table for its subscription implementation with Stripe and Braintree I cannot recreate the table and the reason I just modified it to add the additional fields that were not apart of the table.

--- src/Migrations/2014_10_12_200000_create_subscriptions_table.php
+++ src/Migrations/2014_10_12_200000_create_subscriptions_table.php
@@ -11,7 +11,7 @@ class CreateSubscriptionsTable extends Migration
      *
      * @return void
      */
-    public function up()
+    /**public function up()
     {
         Schema::create('subscriptions', function($table) {
             $table->increments('id');
@@ -25,6 +25,20 @@ class CreateSubscriptionsTable extends Migration
             $table->timestamp('next_billing_at')->nullable();
             $table->timestamps();
         });
+    }**/
+    
+    /**
+      * Run the migrations.
+      *
+      * @return void
+      */
+    public function up() {
+        Schema::table('subscriptions', function ($table) {
+            $table->string('subscription_id');
+            $table->string('plan_id');
+            $table->integer('last_four')->nullable();
+            $table->timestamp('next_billing_at')->nullable();
+        });
     }
 
     /**
@@ -34,6 +48,20 @@ class CreateSubscriptionsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('subscriptions');
+        //Schema::drop('subscriptions');
+        Schema::table('subscriptions', function (Blueprint $table) {
+            if(Schema::hasColumn('subscriptions', 'subscription_id')){
+                $table->dropColumn('subscription_id');
+            }
+            if(Schema::hasColumn('subscriptions', 'plan_id')){
+                $table->dropColumn('plan_id');
+            }
+            if(Schema::hasColumn('subscriptions', 'last_four')){
+                $table->dropColumn('last_four');
+            }
+            if(Schema::hasColumn('subscriptions', 'next_billing_at')){
+                $table->dropColumn('next_billing_at');
+            }
+        });
     }
 }
